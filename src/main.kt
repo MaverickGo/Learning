b67722df020c0d2193d92d581752af742db205c2
MaverickGo <amir.orlovskikh@gmail.com>
2026-02-08 15:21:42 +0200

    Added tutor

--- src/main.kt
+++ src/main.kt
@@ -1,9 +1,14 @@
 import java.lang.IO.println
 
+const val constant: Int = 1; // константа
 fun main(){
     println("Hello World")
+    val releaseYear: Int = 2003     // переменная только для чтения
+    var age: Int = 26               // переменная
+    println("Release $releaseYear, age -> $age")
+    println("2027 is coming")
+    age++;
+    println("Age is $age now")
 
-    var age: Int;
-    age = 23;
-    println(age)
+
 }
