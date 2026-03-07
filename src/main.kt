58efbe8f4ed09d176701cceb9396a7f5ef018784
MaverickGo <amir.orlovskikh@gmail.com>
2026-03-07 11:13:33 +0200

    Solves 3 tasks more

--- src/main.kt
+++ src/main.kt
@@ -1,8 +1,4 @@
-import java.lang.IO.println
-
-const val constant: Int = 1; // константа
 fun main(){
-    // leetCodeTasks
     fun twoSum(nums: IntArray, target: Int): IntArray {
         val result: IntArray = IntArray(2)
         for (i: Int in 0..nums.size-1) {
@@ -15,7 +11,6 @@ fun main(){
         }
         return result
     }
-
     fun missingNumber(nums: IntArray): Int {
         for (i: Int in 0..nums.size) {
             if (!(i in nums)) {
@@ -23,5 +18,48 @@ fun main(){
             }
         }
         return 0
+    }
+    fun singleNumber(nums: IntArray): Int {
+        var res: Int = nums[0];
+        for (i in 1..nums.size-1){
+            res = res xor nums[i];
+        }
+        return res;
+    }
+    fun sortedSquares(nums: IntArray): IntArray {
+        var temp: Int
+        for (i in 0..nums.size-1){
+            nums[i] *= nums[i]
+            if (i == 0) { continue }
+            temp = nums[i]
+            for (j in i-1 downTo 0) {
+                if (nums[j] > temp){
+                    nums[j+1] = nums[j]
+                    nums[j] = temp
+                } else {
+                    break
+                }
+            }
+        }
+        return nums
+    }
+    fun moveZeroes(nums: IntArray): Unit {
+        var temp: Int
+        for (i in nums.size-1 downTo 0) {
+            if (nums[i] != 0) { continue }
+            for (j in i+1..nums.size-1) {
+                if (nums[j] == 0) {
+                    nums[j-1] = 0
+                    break
+                }
+                if (j == nums.size-1) {
+                    nums[j-1] = nums[j]
+                    nums[j] = 0
+                    break
+                }
+                nums[j-1] = nums[j]
+                nums[j] = 0
+            }
+        }
     }
 }
